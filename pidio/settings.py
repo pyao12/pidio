63b60a3cf912f3c887b9893a912917e7007e6192
patrickyao <patrick25yao@outlook.com>
2026-01-17 19:05:06 +0800

    设置debug关闭，开放所有访问

--- pidio/settings.py
+++ pidio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%gifci(l20#&2@*n*e14^#s$u=h@m2p25hmsbw-db6(ar4od_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
